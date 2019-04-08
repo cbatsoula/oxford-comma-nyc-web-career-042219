5f0ff11a5f31e68d01ac9bee639093122d1e9d47
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-08 20:38:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
+
+
 def oxford_comma(array)
-array.join(",")
-array.join(" and ")
-if array.length == 3
-  array[-1] << " and "
+  if array.length == 2
+  array[-2] << " and "
   array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
